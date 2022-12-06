05e54b1546bdfe43d2bbe039245a1b678f92322c
tonytutoring <102200546+tonytutoring@users.noreply.github.com>
2022-12-06 15:01:47 +0900

    글로벌 스타일 파일추가

---
+++ src/ErrorBoundary.tsx
@@ -0,0 +1,35 @@
+import React, { Component, ReactNode } from "react";
+import { NavigateFunction } from 'react-router-dom';
+
+interface IErrorBoundaryProps {
+  children: ReactNode;
+  navigate: NavigateFunction;
+}
+
+interface IErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component {
+  constructor(props: IErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    logErrorToMyService(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>
+    }
+
+    return this.props.children;
+  }
+}
+
